3db8b3520b9c364f1b680c692485cef6d21a0679
Peter Karman <peter@peknet.com>
2022-05-19 17:54:45 -0500

    fix tests

--- ksvotes/services/tests/test_form_filler_service.py
+++ ksvotes/services/tests/test_form_filler_service.py
@@ -3,16 +3,19 @@ import json
 import re
 import base64
 from ksvotes.services.form_filler_service import FormFillerService
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-def test_vr_en_form(app, db_session, client):
-    payload_file = "app/services/tests/test-vr-en-payload.json"
+def test_vr_en_form():
+    payload_file = "ksvotes/services/tests/test-vr-en-payload.json"
     with open(payload_file) as payload_f:
         payload = json.load(payload_f)
 
         ffs = FormFillerService(payload=payload, form_name="/vr/en")
         img = ffs.as_image()
-        app.logger.info("got image:{}".format(img))
+        logger.info("got image:{}".format(img))
         matches = re.fullmatch(r"(data:image\/(.+?);base64),(.+)", img, re.I)
 
         assert matches.group(1) == "data:image/png;base64"
