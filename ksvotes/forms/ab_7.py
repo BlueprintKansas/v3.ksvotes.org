e76edd5bbae28773614d9e04ec8ad8f45d52333e
Peter Karman <peter@peknet.com>
2022-05-20 15:06:45 -0500

    djangoify

--- ksvotes/forms/ab_7.py
+++ ksvotes/forms/ab_7.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-from flask_wtf import FlaskForm
-from wtforms import BooleanField
+from wtforms import Form, BooleanField
 from wtforms.validators import DataRequired
-from flask_babel import lazy_gettext
+from django.utils.translation import gettext_lazy as lazy_gettext
 
 
-class FormAB7(FlaskForm):
+class FormAB7(Form):
     affirmation = BooleanField(
         lazy_gettext("AB7_affirm"),
         validators=[DataRequired(message=lazy_gettext("Required"))],
--- ksvotes/views/ab/AB7_affirmation.py
+++ ksvotes/views/ab/AB7_affirmation.py
@@ -1,68 +1,80 @@
 # -*- coding: utf-8 -*-
-from app.main import main
-from flask import g, url_for, render_template, redirect, request
-from flask_babel import lazy_gettext
-from app import db
-from app.models import Registrant, Clerk
-from app.decorators import InSession
-from app.services import SessionManager
-from app.services.nvris_client import NVRISClient
-from app.services.county_mailer import CountyMailer
-from app.services.id_action_mailer import IdActionMailer
-from app.services.steps import Step_AB_7
-from app.main.forms import FormAB7, CountyPicker
-from datetime import datetime
+from ksvotes.services.session_manager import SessionManager
+from ksvotes.services.county_mailer import CountyMailer
+from ksvotes.services.id_action_mailer import IdActionMailer
+from ksvotes.services.steps import Step_AB_7
+from ksvotes.forms.ab_7 import FormAB7
+from ksvotes.forms.county_picker import CountyPicker
+from django.views.generic import TemplateView
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.utils import timezone
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-@main.route("/ab/affirmation", methods=["GET", "POST"])
-@InSession
-def ab7_affirmation():
-    reg = g.registrant
-    form = FormAB7()
-    clerk = reg.try_clerk()
-    county_picker = CountyPicker()
+class AB7View(TemplateView):
+    template_name = "ab/affirmation.html"
 
-    # if we don't have a signed AB form to affirm, redirect
-    if not reg.try_value("ab_forms", False):
-        if not reg.try_value("signature_string", False):
-            return redirect(url_for("main.index"))
-        else:
-            return redirect(url_for("main.ab6_preview_sign"))
+    def get_form(self):
+        return FormAB7()
 
-    ab_forms = reg.try_value("ab_forms")
+    def post(self, request, *args, **kwargs):
+        form = FormAB7(request.POST)
+        if form.validate():
+            step = Step_AB_7(form.data)
+            reg = request.registrant
+            clerk = reg.try_clerk()
 
-    if request.method == "POST" and form.validate_on_submit():
-        step = Step_AB_7(form.data)
-        if step.run():
-            reg.update(form.data)
-            reg.save(db.session)
+            # if we don't have a signed AB form to affirm, redirect
+            if not reg.try_value("ab_forms", False):
+                if not reg.try_value("signature_string", False):
+                    return redirect(reverse("ksvotes:home.index"))
+            else:
+                return redirect(reverse("ksvotes:ab.preview"))
 
-            mailer = CountyMailer(reg, clerk, "ab_forms")
-            r = mailer.send()
+            if step.run():
+                reg.update(form.data)
+                reg.save()
 
-            # if there was no ID string defined, send the action-needed email
-            if not reg.ab_permanent and not reg.try_value("ab_identification"):
-                id_action_mailer = IdActionMailer(reg, clerk)
-                resp = id_action_mailer.send()
-                reg.update({"ab_id_action_email_sent": resp["MessageId"]})
+                mailer = CountyMailer(reg, clerk, "ab_forms")
+                r = mailer.send()
 
-            # any error gets a special page
-            for k in ["clerk", "receipt"]:
-                if k not in r or "MessageId" not in r[k] or not r[k]["MessageId"]:
-                    # TODO log New Relic event
-                    return render_template("email_error.html", clerk=clerk)
+                # if there was no ID string defined, send the action-needed email
+                if not reg.ab_permanent and not reg.try_value("ab_identification"):
+                    id_action_mailer = IdActionMailer(reg, clerk)
+                    resp = id_action_mailer.send()
+                    reg.update({"ab_id_action_email_sent": resp["MessageId"]})
 
-            reg.update({"ab_forms_message_id": r["clerk"]["MessageId"]})
-            reg.ab_completed_at = datetime.utcnow()
-            reg.save(db.session)
+                # any error gets a special page
+                for k in ["clerk", "receipt"]:
+                    if k not in r or "MessageId" not in r[k] or not r[k]["MessageId"]:
+                        # TODO log New Relic event
+                        return render(request, "email_error.html", {"clerk": clerk})
+
+                reg.update({"ab_forms_message_id": r["clerk"]["MessageId"]})
+                reg.ab_completed_at = timezone.now()
+                reg.save()
 
             session_manager = SessionManager(reg, step)
             return redirect(session_manager.get_redirect_url())
+        else:
+            logger.debug("FormAB7 failed to validate: {}".format(form.errors))
 
-    return render_template(
-        "ab/affirmation.html",
-        preview_imgs=ab_forms,
-        form=form,
-        clerk=clerk,
-        county_picker=county_picker,
-    )
+        # if anything failed to validate or run, re-render
+        return render(
+            request,
+            self.template_name,
+            self.get_context_data(**kwargs) | {"form": form},
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = self.get_form()
+        context["current_step"] = 7
+        context["previous_step_url"] = reverse("ksvotes:ab.preview")
+        context["preview_imgs"] = self.request.registrant.try_value("ab_forms")
+        context["clerk"] = self.request.registrant.try_clerk()
+        context["county_picker"] = CountyPicker()
+        return context
