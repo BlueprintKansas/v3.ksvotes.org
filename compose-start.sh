35fd158761faf12dd5ae554b0ad1da86de9d4a18
Peter Karman <peter@peknet.com>
2022-05-28 09:51:28 -0500

    built image does not have ./docker

--- compose-start.sh
+++ compose-start.sh
@@ -4,7 +4,7 @@
 # for use with docker-compose.  In deployed or production scenarios you would
 # not necessarily use this exact setup.
 #
-./docker/wait-for-it.sh -h db -p 5432 -t 20 -- python manage.py migrate --noinput
+wait-for-it -h db -p 5432 -t 20 -- python manage.py migrate --noinput
 
 python manage.py collectstatic --noinput
 
