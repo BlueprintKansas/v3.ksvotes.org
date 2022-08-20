1206ba699a62c67979d762e39b82123b5ee05b5d
Peter Karman <karpet@peknet.com>
2022-08-19 21:44:55 -0500

    fix stats timezone, js math

--- ksvotes/services/registrant_stats.py
+++ ksvotes/services/registrant_stats.py
@@ -27,9 +27,9 @@ class RegistrantStats:
         if not end_date:
             end_date = today
         sql = """
-      select cast(vr_completed_at at time zone 'utc' at time zone 'america/chicago' as date) as vr_date, count(id)
+      select cast(vr_completed_at at time zone 'america/chicago' as date) as vr_date, count(id)
       from ksvotes_registrant
-      where vr_completed_at is not null and vr_completed_at at time zone 'utc' at time zone 'america/chicago' between %s and %s
+      where vr_completed_at is not null and vr_completed_at between %s and %s
       group by vr_date
       order by 1
     """
@@ -42,9 +42,9 @@ class RegistrantStats:
         if not end_date:
             end_date = today
         sql = """
-      select cast(ab_completed_at at time zone 'utc' at time zone 'america/chicago' as date) as ab_date, count(id)
+      select cast(ab_completed_at at time zone 'america/chicago' as date) as ab_date, count(id)
       from ksvotes_registrant
-      where ab_completed_at is not null and ab_completed_at at time zone 'utc' at time zone 'america/chicago' between %s and %s
+      where ab_completed_at is not null and ab_completed_at between %s and %s
       group by ab_date
       order by 1
     """
@@ -57,9 +57,9 @@ class RegistrantStats:
         if not end_date:
             end_date = today
         sql = """
-      select cast(created_at at time zone 'utc' at time zone 'america/chicago' as date) as lookup_date, count(id)
+      select cast(created_at at time zone 'america/chicago' as date) as lookup_date, count(id)
       from ksvotes_registrant
-      where reg_lookup_complete is true and created_at at time zone 'utc' at time zone 'america/chicago' between %s and %s
+      where reg_lookup_complete is true and created_at between %s and %s
       group by lookup_date
       order by 1
     """
@@ -72,9 +72,9 @@ class RegistrantStats:
         if not end_date:
             end_date = today
         sql = """
-      select cast(created_at at time zone 'utc' at time zone 'america/chicago' as date) as lookup_date, count(id)
+      select cast(created_at at time zone 'america/chicago' as date) as lookup_date, count(id)
       from ksvotes_registrant
-      where reg_found is true and created_at at time zone 'utc' at time zone 'america/chicago' between %s and %s
+      where reg_found is true and created_at between %s and %s
       group by lookup_date
       order by 1
     """
