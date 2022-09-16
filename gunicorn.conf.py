d1b57732627431c4952710b97d51d21d48218e8f
Peter Karman <karpet@peknet.com>
2022-09-16 09:24:31 -0500

    use max_requests to mitigate mem leak

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -15,6 +15,8 @@ workers = int(os.environ.get("WEB_CONCURRENCY", "2"))
 worker_class = "gevent"
 keepalive = 32
 worker_connections = 10000
+# mitigate known memory leak (likely in image preview)
+max_requests = 1000
 
 pythonpath = BASE_DIR
 chdir = BASE_DIR
