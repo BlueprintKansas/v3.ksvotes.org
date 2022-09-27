74fd1ae521a9b7400247b56fd663fe2277aced2f
Peter Karman <karpet@peknet.com>
2022-09-26 19:44:06 -0500

    redirect /favicon.ico to actual file

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
 from django.conf.urls.i18n import i18n_patterns
+from django.views.generic import RedirectView
 from rest_framework import routers
 from users.views import UserViewSet, CurrentUserView
 from ak.views import (
@@ -20,6 +21,9 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/me/", CurrentUserView.as_view(), name="current-user"),
     re_path(r"^api/v1/", include(router.urls)),
+    path(
+        "favicon.ico", RedirectView.as_view(url="/static/img/favicon/favicon-96x96.png")
+    ),
     path("200", OKView.as_view(), name="ok"),
     path("403", ForbiddenView.as_view(), name="forbidden"),
     path("404", NotFoundView.as_view(), name="not_found"),
