3caf3ed13bc52b9c8102e242a92e2e120294e4cb
Peter Karman <karpet@peknet.com>
2022-10-05 10:21:11 -0500

    add nginx proxy to serve static files, buffer in front of gunicorn (#56)
    * add nginx proxy to serve static files, buffer in front of gunicorn
    * fix nginx perms

--- compose-start.sh
+++ compose-start.sh
@@ -9,4 +9,14 @@ set -x
 
 make migrate static locales fixtures
 
-newrelic-admin run-program gunicorn -c gunicorn.conf.py --reload -b 0.0.0.0:${PORT:=8000} config.wsgi
+# must update $PORT from heroku at runtime
+NGINX_PORT=${PORT:=8000}
+NGINX_CONF=/etc/nginx/nginx.conf
+sed -i -e 's/set \$PORT/# set \$PORT/g' $NGINX_CONF
+sed -i -e 's/\$PORT/'"$NGINX_PORT"'/g' $NGINX_CONF
+
+# start proxy first
+nginx -c $NGINX_CONF -g 'daemon on;'
+
+# run app last
+newrelic-admin run-program gunicorn -c gunicorn.conf.py --reload -b 0.0.0.0:${APP_PORT:=8001} config.wsgi
