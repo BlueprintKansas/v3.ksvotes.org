75673aec0c0bb0867017449b04400cc178a91e9b
Peter Karman <karpet@peknet.com>
2022-10-19 12:51:26 -0500

    debug confirmed

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -12,9 +12,6 @@ capture_output = True
 bind = "unix:/run/gunicorn.sock"
 loglevel = "INFO"
 workers = int(os.environ.get("WEB_CONCURRENCY", "2"))
-
-print("concurrency=={}".format(workers))
-
 worker_class = "gevent"
 keepalive = 32
 worker_connections = 10000
