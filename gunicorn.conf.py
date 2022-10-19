6825eabdc45ae99c9f7d91def6ea079f21842783
Peter Karman <karpet@peknet.com>
2022-10-19 11:37:50 -0500

    longer-lived gunicorn workers since memory leak is fixed

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -16,8 +16,7 @@ workers = int(os.environ.get("WEB_CONCURRENCY", "2"))
 worker_class = "gevent"
 keepalive = 32
 worker_connections = 10000
-# mitigate known memory leak (likely in image preview)
-max_requests = 1000
+max_requests = 10000
 
 pythonpath = BASE_DIR
 chdir = BASE_DIR
