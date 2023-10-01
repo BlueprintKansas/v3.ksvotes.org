f43f616a5d9f0b9ddc978fde78053c1e83e4a6fd
Peter Karman <karpet@peknet.com>
2023-10-01 10:58:31 -0500

    fix test

--- playwright/test_php_deny.py
+++ playwright/test_php_deny.py
@@ -2,4 +2,4 @@
 def test_php_deny(page):
     # all requests that end in .php should be denied
     response = page.goto("/some/path/to/file.php")
-    assert response.status == 400
+    assert response.status == 403
