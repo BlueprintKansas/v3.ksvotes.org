32f7fae5653cb89a04b27d05748c2214af0766de
Peter Karman <karpet@peknet.com>
2025-08-18 12:56:08 +0000

    address could be None

--- ksvotes/services/usps_api.py
+++ ksvotes/services/usps_api.py
@@ -17,7 +17,8 @@ class DummyClient:
     """Use for CI tests when we don't want to ping USPS"""
 
     def standardize(self, address: dict[str, str]) -> dict[str, str]:
-        if address.get("address_extended", "").startswith("Room"):
+        unit = address.get("address_extended", "") or ""
+        if unit and unit.startswith("Room"):
             address["address_extended"] = address["address_extended"].replace(
                 "Room", "RM"
             )
