a7eb789a63db19bcaad18013f27cc83ea0941d3d
Peter Karman <karpet@peknet.com>
2026-07-02 06:22:32 -0500

    test usps api (#637)

--- ksvotes/templatetags/utils.py
+++ ksvotes/templatetags/utils.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.template.defaulttags import register
 import json
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @register.filter
@@ -10,7 +13,7 @@ def get_item(dictionary, key):
 
 @register.filter
 def get_item_default(dictionary, key, default=""):
-    return dictionary.get(key, default)
+    return dictionary.get(key, default) or default
 
 
 @register.filter
